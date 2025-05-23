db069a00edb2a211b96b3399ea1d3c8204536942
Guilherme Rocha <luzguilherme.dev@gmail.com>
2025-05-23 16:33:05 -0300

    adicionando lógica de busca por outros meios além do nome

--- main.js
+++ main.js
@@ -1,66 +1,133 @@
-const time = document.getElementById("input-times");
+const searchInput = document.getElementById("input-times");
 const search = document.getElementById("search");
 const foundJogos = document.getElementById("foundGames");
+const nextJogos = document.getElementById("nextGames");
+const labelFoundJogos = document.getElementById("labelFoundJogos");
+const labelNextJogos = document.getElementById("labelNextJogos");
+const select = document.getElementById("select");
 
-search.addEventListener("click", () => {
-  const timeNome = time.value;
+const getJogos = (shearchType) => {
+  const inputValue = searchInput.value;
+  if (!inputValue) {
+    searchInput.classList.add("alert");
+    setTimeout(() => {
+      searchInput.classList.remove("alert");
+    }, 2000);
+    return;
+  }
 
-  const res = fetch("http://localhost:3000/jogos/nome/" + timeNome)
+  fetch(`http://localhost:3000/jogos/${shearchType}/${inputValue}`)
     .then((res) => {
       return res.json();
     })
     .then((res) => {
-      listJogos(res);
+      listJogos(res, foundJogos, labelFoundJogos);
     });
+};
+
+const handlePlaceholder = () => {
+  switch (select.value) {
+    case "nome":
+      searchInput.placeholder = "Busque os jogos pelo nome do time...";
+      break;
+    case "rodada":
+      searchInput.placeholder = "Busque os jogos pelo numero da rodada...";
+      break;
+    case "estadio":
+      searchInput.placeholder = "Busque os jogos pelo estadio...";
+      break;
+    default:
+      searchInput.placeholder = "Busque os jogos pelo campeonato...";
+      break;
+  }
+};
+
+select.addEventListener("change", () => {
+  searchInput.value = "";
+  handlePlaceholder();
 });
 
-const listJogos = (jogos) => {
+const handleSelect = () => {
+  handlePlaceholder();
+  switch (select.value) {
+    case "nome":
+      getJogos("nome");
+      break;
+    case "rodada":
+      getJogos("rodada");
+      break;
+    case "estadio":
+      getJogos("estadio");
+      break;
+    default:
+      getJogos("campeonato");
+      break;
+  }
+};
+
+searchInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    handleSelect();
+  }
+});
+search.addEventListener("click", () => {
+  handleSelect();
+});
+
+const listJogos = (jogos, foundJogos, label) => {
   foundJogos.innerHTML = "";
-  jogos.forEach((jogo) => {
+  label.innerHTML =
+    label.id == "labelFoundJogos" ? "Jogos encontrados:" : "Próximos jogos:";
+
+  if (jogos.length == 0) {
+    foundJogos.innerHTML =
+      "<p class='feedback'>Não encontramos nenhum jogo!</p>";
+  }
+  jogos.forEach((jogo, i) => {
     foundJogos.insertAdjacentHTML(
       "beforeend",
       `
-      <li class="card">
-        <div class="card-header">
-          <span>
-            <i class="bi bi-calendar2-check"></i>
-             ${returnData(jogo.data)}
-          </span>
-          <span>
-            <i class="bi bi-alarm"></i>
-            ${jogo.hora}
-          </span>
+      <li class="card" style="animation-delay: ${i * 0.1}s;">
+      <div class="card-header">
+        <span>
+        <i class="bi bi-calendar2-check"></i>
+         ${returnData(jogo.data)}
+        </span>
+        <span>
+        <i class="bi bi-alarm"></i>
+        ${jogo.hora}
+        </span>
+      </div>
+       <h3 class="rodada">${jogo.rodada}ª Rodada</h3>
+      <div class="card-clubes">
+        <div class="card-club">
+        <img class="card-img-club" src="${jogo.time1.escudo}" alt="" />
+        <p>${jogo.time1.nome}</p>
         </div>
-         <h3 class="rodada">${jogo.rodada}ª Rodada</h3>
-        <div class="card-clubes">
-          <div class="card-club">
-            <img class="card-img-club" src="${jogo.time1.escudo}" alt="" />
-            <p>${jogo.time1.nome}</p>
-          </div>
-          <img class="card-icon" src="./images/x.png" alt="" />
-          <div class="card-club">
-            <img class="card-img-club" src="${jogo.time2.escudo}" alt="" />
-            <p>${jogo.time2.nome}</p>
-          </div>
+        <img class="card-icon" src="./images/x.png" alt="" />
+        <div class="card-club">
+        <img class="card-img-club" src="${jogo.time2.escudo}" alt="" />
+        <p>${jogo.time2.nome}</p>
         </div>
-        <div class="card-infos">
-          <div class="info-trasmissao">
-            <i class="bi bi-tv"></i>
-            <span class="info">Transmissão:</span>
-            <span class="info-canais">${
-              Array.isArray(jogo.canal) ? jogo.canal.join(", ") : jogo.canal
-            }</span>
-          </div>
-          <div class="info-estadio">
-             <i class="bx bx-stadium"></i>
-            <span class="info">Estádio:</span>
-            <span class="info-estadio">${jogo.estadio}</span>
-          </div>
-          <div class="info-trasmissao">
-            <i class="bi bi-trophy"></i>
-            <span class="info">${jogo.campeonato}</span>
-          </div>
+      </div>
+      <div class="card-infos">
+        <div class="info-trasmissao">
+        <i class="bi bi-tv"></i>
+        <span class="info">Transmissão:</span>
+        <span class="info-canais">${
+          Array.isArray(jogo.canal) ? jogo.canal.join(", ") : jogo.canal
+        }</span>
         </div>
+        <div class="info-estadio">
+         <i class="bx bx-stadium"></i>
+        <span class="info">Estádio:</span>
+        <span class="info-estadio">${jogo.estadio}</span>
+        </div>
+        <div class="info-trasmissao">
+        <i class="bi bi-trophy"></i>
+        <span class="info">${jogo.campeonato}</span>
+        </div>
+      </div>
       </li>
     `
     );
@@ -97,3 +164,21 @@ const returnData = (data) => {
   }
   return `${dataFormatada.getDate()} ${meses[dataFormatada.getMonth()]}`;
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  handlePlaceholder();
+  const hoje = new Date();
+  const ano = hoje.getFullYear();
+  const mes = String(hoje.getMonth() + 1).padStart(2, "0");
+  const dia = String(hoje.getDate()).padStart(2, "0");
+  const dataAtual = `${ano}-${mes}-${dia}`;
+  const resposta = fetch("http://localhost:3000/jogos/data/" + dataAtual);
+
+  resposta
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      listJogos(res, nextJogos, labelNextJogos);
+    });
+});
--- script.js
+++ script.js
@@ -2,38 +2,96 @@ const jogos = require("./jogos");
 
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/jogos/id/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const jogo = jogos.find((c) => c.id === id);
-  if (jogo) {
-    res.json(jogo);
-  } else {
-    res.status(404).json({ error: "Jogo não encontrado" });
-  }
-});
-
-app.get("/jogos", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
+// End Point de busca por nomes;
 app.get("/jogos/nome/:nome", (req, res) => {
   const nome = req.params.nome;
-  const jogosEncontrados = jogos.filter(
+  let jogosEncontrados = jogos.filter(
     (c) =>
       c.time1.nome.toLowerCase() === nome.toLowerCase() ||
-      c.time2.nome.toLowerCase() === nome.toLowerCase()
+      (c.time2.nome.toLowerCase() === nome.toLowerCase() &&
+        new Date(c.data) >= new Date())
   );
+
   if (jogosEncontrados.length > 0) {
     res.json(jogosEncontrados);
   } else {
+    res.json(jogosEncontrados);
     res.status(404).json({ error: "Jogo não encontrado" });
   }
+});
+
+// End Point de busca por campeonato
+app.get("/jogos/campeonato/:campeonato", (req, res) => {
+  const campeonato = req.params.campeonato;
+  let jogosEncontrados = jogos.filter(
+    (c) =>
+      c.campeonato.toLowerCase() === campeonato.toLowerCase() &&
+      new Date(c.data) >= new Date()
+  );
+
+  if (jogosEncontrados.length > 0) {
+    res.json(jogosEncontrados);
+  } else {
+    res.json(jogosEncontrados);
+    res.status(404).json({ error: "Jogo não encontrado" });
+  }
+});
+
+// End point de busca por rodada;
+app.get("/jogos/rodada/:rodada", (req, res) => {
+  const rodada = req.params.rodada;
+  // Verifica se rodada é um número válido
+  if (isNaN(Number(rodada))) {
+    res.json({});
+    return res.status(400).json({ error: "Rodada deve ser um número" });
+  }
+  let jogosEncontrados = jogos.filter(
+    (c) => Number(c.rodada) === Number(rodada) && new Date(c.data) >= new Date()
+  );
+
+  if (jogosEncontrados.length > 0) {
+    res.json(jogosEncontrados);
+  } else {
+    res.json(jogosEncontrados);
+    res.status(404).json({ error: "Jogo não encontrado" });
+  }
+});
+
+// End point de busca por estádio;
+app.get("/jogos/estadio/:estadio", (req, res) => {
+  const estadio = req.params.estadio;
+  let jogosEncontrados = jogos.filter(
+    (c) =>
+      c.estadio.toLowerCase() === estadio.toLowerCase() &&
+      new Date(c.data) >= new Date()
+  );
+
+  if (jogosEncontrados.length > 0) {
+    res.json(jogosEncontrados);
+  } else {
+    res.json(jogosEncontrados);
+    res.status(404).json({ error: "Jogo não encontrado" });
+  }
+});
+
+app.get("/jogos/data/:data", (req, res) => {
+  const data = req.params.data;
+  const jogosProximos = [];
+  let currentData = new Date();
+  let nextWeek = new Date(currentData);
+  nextWeek.setDate(currentData.getDate() + 6);
+  for (let j = 0; j < jogos.length; j++) {
+    const dataJogo = new Date(jogos[j].data);
+    if (dataJogo <= nextWeek && dataJogo >= currentData) {
+      jogosProximos.push(jogos[j]);
+    }
+  }
+  res.json(jogosProximos);
 });
 
 app.listen(port, () => {
